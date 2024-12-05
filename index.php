f232b3a0cdd9ac0160080a3ea88add5293559377
KevSyndrome <axelrami422@gmail.com>
2024-12-05 17:34:12 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a> INICIO APP </a>
+    <a href="html/index.php" class="btn">BOTÓN</a>
+    <button type="button" onclick="window.location.href='html/index.php';">BOTÓN</button>
+
+</body>
+</html>
